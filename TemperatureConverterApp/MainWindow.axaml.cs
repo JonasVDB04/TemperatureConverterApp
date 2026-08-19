719e7bd1c230e27acd386ca5f411110a98193fbf
Jonas <jonas@2doo.be>
2026-08-19 14:58:26 +0200

    Base temperature converter app done

--- TemperatureConverterApp/MainWindow.axaml.cs
+++ TemperatureConverterApp/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace TemperatureConverterApp;
 
@@ -7,5 +8,19 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+    
+    private void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if (double.TryParse(Celsius.Text, out double celsius))
+        {
+            double fahrenheit = celsius * (9d / 5d) + 32;
+            Fahrenheit.Text = fahrenheit.ToString("0.0");
+        }
+        else
+        {
+            Celsius.Text = "0";
+            Fahrenheit.Text = "0";
+        }
     }
 }
