9fcd4f0cfbb61abf25223d8eb2457e73406b0767
Jonas <jonas@2doo.be>
2026-08-19 15:13:01 +0200

    Avalonia tutorial done

--- TemperatureConverterApp/MainWindow.axaml.cs
+++ TemperatureConverterApp/MainWindow.axaml.cs
@@ -10,9 +10,13 @@ public partial class MainWindow : Window
         InitializeComponent();
     }
     
-    private void Button_OnClick(object? sender, RoutedEventArgs e)
+    private void Celsius_TextChanged(object? sender, RoutedEventArgs e)
     {
-        if (double.TryParse(Celsius.Text, out double celsius))
+        if (string.IsNullOrEmpty(Celsius.Text) || Celsius.Text == "-")
+        {
+            Fahrenheit.Text = "";
+        }
+        else if (double.TryParse(Celsius.Text, out double celsius))
         {
             double fahrenheit = celsius * (9d / 5d) + 32;
             Fahrenheit.Text = fahrenheit.ToString("0.0");
